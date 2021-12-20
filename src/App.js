aee6aeb6eb84bb715102fee324bde4e8f2fed696
yomoon93 <kevmunar@hotmail.com>
2021-12-19 22:56:11 -0500

    still deleting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import NavBar from './Components/NavBar/navbar';
-import ParticlesString from './Components/Particles/particles';
+
 import Main from './Pages/home/main';
 import Skills from './Components/Skills/skills';
 import Flipskills from './Pages/Flip/flipskills';
@@ -10,7 +10,7 @@ import Footer from './Components/Footer/footer';
 function App() {
   return (
     <div className="App">
-      {/* <ParticlesString/> */}
+    
       <NavBar/>
       <Main/>
       <Skills/>
